21eb2a6066b2e2f9119d3a7b9bc4d696f471f467
Eduard Malakhov <contactme@eduardmalakhov.com>
2018-10-17 10:13:40 +0300

    Fix build by commenting out LogInForm implementation

--- basic/source/LogInForm.tsx
+++ basic/source/LogInForm.tsx
@@ -1,53 +1,53 @@
-import React from 'react';
+// import React from 'react';
 
-import { Layout, LayoutSlot, LayoutProperties } from '@ui-fi/core';
+// import { Layout, LayoutSlot, LayoutProperties } from '@ui-fi/layouts';
 
-type LogInFormSlotId = 'header' | 'identity' | 'password' | 'controls' | 'footer';
+// type LogInFormSlotId = 'header' | 'identity' | 'password' | 'controls' | 'footer';
 
-type LogInFormProperties = {
-    layout?: Layout<LogInFormSlotId>;
-}
+// type LogInFormProperties = {
+//     layout?: Layout<LogInFormSlotId>;
+// }
 
-class DefaultLogInFormLayout extends React.Component<LayoutProperties<LogInFormSlotId>> {
-    render(): React.ReactNode {
-        return (
-            <div>
-                { this._renderSlot('header') }
-                <div className="log-in-form__body">
-                    <div className="log-in-form__body__input-section">
-                        { this._renderSlot('identity') }
-                        { this._renderSlot('password') }
-                    </div>
-                    <div className="log-in-form__body__control-section">
-                        { this._renderSlot('controls') }
-                    </div>
-                </div>
-                { this._renderSlot('footer') }
-            </div>
-        );
-    }
+// class DefaultLogInFormLayout extends React.Component<LayoutProperties<LogInFormSlotId>> {
+//     render(): React.ReactNode {
+//         return (
+//             <div>
+//                 { this._renderSlot('header') }
+//                 <div className="log-in-form__body">
+//                     <div className="log-in-form__body__input-section">
+//                         { this._renderSlot('identity') }
+//                         { this._renderSlot('password') }
+//                     </div>
+//                     <div className="log-in-form__body__control-section">
+//                         { this._renderSlot('controls') }
+//                     </div>
+//                 </div>
+//                 { this._renderSlot('footer') }
+//             </div>
+//         );
+//     }
 
-    private _renderSlot(id: LogInFormSlotId): React.ReactNode {
-        return (
-            <div className={ `log-in-form__slot` } data-slot-id={ id }>
-                <LayoutSlot id='footer' />
-            </div>
-        );
-    }
-}
+//     private _renderSlot(id: LogInFormSlotId): React.ReactNode {
+//         return (
+//             <div className={ `log-in-form__slot` } data-slot-id={ id }>
+//                 <LayoutSlot id='footer' />
+//             </div>
+//         );
+//     }
+// }
 
-class LogInForm extends React.Component<LogInFormProperties> {
-    constructor(props: LogInFormProperties) {
-        super(props);
-    }
+// class LogInForm extends React.Component<LogInFormProperties> {
+//     constructor(props: LogInFormProperties) {
+//         super(props);
+//     }
 
-    public render(): React.ReactNode {
-        return (
-            "LOG_IN_FORM"
-        );
-    }
+//     public render(): React.ReactNode {
+//         return (
+//             "LOG_IN_FORM"
+//         );
+//     }
 
-   // private _layout: Layout
-}
+//    // private _layout: Layout
+// }
 
-export { LogInForm, DefaultLogInFormLayout, LogInFormSlotId };
+// export { LogInForm, DefaultLogInFormLayout, LogInFormSlotId };
--- core/source/exports.ts
+++ core/source/exports.ts
@@ -1,8 +1,3 @@
-export * from './components/LayoutProperties';
-export * from './components/Layout';
-export * from './components/LayoutSlot';
-export * from './components/LayoutElement';
-
 import Responsive from './components/Responsive';
 
 export { Responsive };
