24dc0ebd26094aca6e7694eabf1d38ffa815c2bb
Eduard Malakhov <contactme@eduardmalakhov.com>
2018-10-18 13:26:01 +0300

    Refactor GuidedContentFrame

--- packages/experimental/source/components/GuidedContentFrame.tsx
+++ packages/experimental/source/components/GuidedContentFrame.tsx
@@ -12,8 +12,10 @@ type TwoPartContentFrameWidth = [ContentFrameWidthGuide, ContentFrameWidthGuide]
 type FourPartContentFrameWidth = [ContentFrameWidthGuide, ContentFrameWidthGuide
     , ContentFrameWidthGuide, ContentFrameWidthGuide];
 
+type ContentFrameWidth = UniformContentFrameWidth | TwoPartContentFrameWidth | FourPartContentFrameWidth;
+
 type GuidedContentFrameProperties = {
-    width: UniformContentFrameWidth | TwoPartContentFrameWidth | FourPartContentFrameWidth;
+    width?: ContentFrameWidth;
 };
 
 const CLASS_NAME_PREFIX = 'ui-fi__frame__width';
@@ -22,24 +24,28 @@ const UNIFORM_CLASS_NAME_SUFFIXES = ['uniform'];
 const TWO_PART_CLASS_NAME_SUFFIXES = ['vertical', 'horizontal'];
 const FOUR_PART_CLASS_NAME_SUFFIXES = ['top', 'right', 'bottom', 'left'];
 
+const SUFFIX_BY_FIELD_LENGTH: { [index: number]: string[] } = {
+    1: UNIFORM_CLASS_NAME_SUFFIXES,
+    2: TWO_PART_CLASS_NAME_SUFFIXES,
+    4: FOUR_PART_CLASS_NAME_SUFFIXES,
+};
+
 class GuidedContentFrame extends React.Component<GuidedContentFrameProperties> {
     constructor(props: GuidedContentFrameProperties) {
         super(props);
 
-        this._width = this.props.width && !Array.isArray(this.props.width)
-            ? [this.props.width]
-            : this.props.width;
+        //  Convert width to an array even when it is a scalar value or undefined
+        //  to make future operations involving with more straighforward.
+        this._width = this.props.width
+            ? !Array.isArray(this.props.width)
+                ? [this.props.width]
+                : this.props.width
+            : ['medium'];
     }
 
     public render(): React.ReactNode {
-        const suffixes = !Array.isArray(this._width)
-            ? UNIFORM_CLASS_NAME_SUFFIXES
-            : this._width.length === 2
-                ? TWO_PART_CLASS_NAME_SUFFIXES
-                : FOUR_PART_CLASS_NAME_SUFFIXES;
-
-        const classes: string[] = suffixes
-            .map((s: string, i: number) => [CLASS_NAME_PREFIX, s, this._width[i]].join('__'));
+        const classes: string[] = this._getClassSuffixes()
+            .map((s: string, i: number) => this._composeClassName(CLASS_NAME_PREFIX, s, this._width[i]));
 
         return (
             <Frame classes={ classes }>
@@ -48,7 +54,21 @@ class GuidedContentFrame extends React.Component<GuidedContentFrameProperties> {
         );
     }
 
-    private _width: UniformContentFrameWidth | TwoPartContentFrameWidth | FourPartContentFrameWidth;
+    private _getClassSuffixes(): string[] {
+        const suffixes = SUFFIX_BY_FIELD_LENGTH[this._width.length];
+
+        if (!suffixes) {
+            throw Error(`Unexpected error: incorrect width ${this._width.length}`);
+        }
+
+        return suffixes;
+    }
+
+    private _composeClassName(prefix: string, suffix: string, value: string): string {
+        return [prefix, suffix, value].join('__');
+    }
+
+    private _width: ContentFrameWidth;
 }
 
 export { GuidedContentFrame };
